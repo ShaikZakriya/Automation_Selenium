8ad8c2b0787aee06a42fca71aac72e7b021a2e75
ShaikZakriya <155053622+ShaikZakriya@users.noreply.github.com>
2025-04-18 18:16:17 +0530

    Code
    Updated

--- src/main/java/Java/First_Code.java
+++ src/main/java/Java/First_Code.java
@@ -6,20 +6,13 @@ public class First_Code {
 		
 		String name = "Automation";
 		int a = 50;
-<<<<<<< Updated upstream
         int b = 100;
 		
 		System.out.println(name);
         System.out.println(a);
         System.out.println(b);
         
-		
-		
-	
-=======
-		System.out.println("a");
-		
->>>>>>> Stashed changes
+
 
 	}
 
