ab4ceb14d8165cce29b19caf286c09b61a7b0e1c
Mustaq Ali Baig <mustaqalibaig3698@gmail.com>
2025-04-29 18:35:15 +0530

    Mustaq
    Properties Data Updated

--- src/main/java/framebuilding_blocks/PropertyFileReader.java
+++ src/main/java/framebuilding_blocks/PropertyFileReader.java
@@ -9,22 +9,45 @@ public class PropertyFileReader {
 
 	public static void main(String[] args) throws IOException {
 		
-		readPersonalData("name");	
-		readPersonalData("age");
-		readPersonalData("State");
-		readPersonalData("Street");
+		//readPersonalData("Name");
+		String browserType = educationData("browserType");
+		System.out.println(browserType);
+//		readPersonalData("age");
+//		readPersonalData("State");
+//		readPersonalData("Street");
 	}
 
+private static String educationData(String key) throws IOException {
+	 Properties prop = new Properties();
+	 String path = System.getProperty("user.dir")+"\\TestData\\EducationData.properties";
+	 FileInputStream fis = new FileInputStream(path);
+	 prop.load(fis);
+		
+	 String value = prop.getProperty(key);
+	 //System.out.println(value);
+	 
+	 return value;
+		
+	}
+
+//	private static void readPersonalData(String key) throws IOException {
+//		
+//		Properties prop = new Properties();
+//		String path = System.getProperty("user.dir")+"\\TestData\\personalData.properties";
+//		File file = new File (path);
+//		FileInputStream fis = new FileInputStream(file);
+//		prop.load(fis);		
+//		System.out.println(prop.get(key));
+//
+//	}
+	
 	private static void readPersonalData(String key) throws IOException {
-		
-		Properties prop = new Properties();
-		String path = System.getProperty("user.dir")+"\\TestData\\personalData.properties";
-		File file = new File (path);
-		FileInputStream fis = new FileInputStream(file);
-		prop.load(fis);		
-		System.out.println(prop.get(key));
-
+	    Properties prop = new Properties();
+	    String path = System.getProperty("user.dir")+"\\TestData\\Educational.properties";
+	    File file = new File(path);
+	    FileInputStream fis = new FileInputStream(file);
+	    prop.load(fis);
+	    System.out.println(prop.getProperty(key));
 	}
-		
-		
 }
+	
