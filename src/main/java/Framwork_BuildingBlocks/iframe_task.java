138a8b3521390ce4adb077885fa5eac3c36818ad
Mustaq Ali Baig <mustaqalibaig3698@gmail.com>
2025-05-06 18:05:16 +0530

    Mustaq
    i frame updated

--- src/main/java/Framwork_BuildingBlocks/iframe_task.java
+++ src/main/java/Framwork_BuildingBlocks/iframe_task.java
@@ -9,16 +9,39 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class iframe_task {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		
-		Switch_To_WebElement(null);
+		Switchs_To_WebElement(null);
 		Switch_To_Index(2);
 		Switch_nameId("zakir");
 		
 		
 		
 
+	}
+
+	private static void Switchs_To_WebElement(WebElement ele) throws InterruptedException {
+		
+		WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.get("https://the-internet.herokuapp.com/iframe");
+       
+        
+        
+        driver.switchTo().frame(ele);
+        
+        driver.findElement(By.xpath("//button[@class=\"tox-notification__dismiss tox-button tox-button--naked tox-button--icon\"]")).click();
+        
+        driver.findElement(By.id("tinymce")).clear();
+        Thread.sleep(2000);
+        
+       // driver.findElement(By.id("tinymce")).sendKeys("ZR Zakir");
+       
+        
+        driver.quit();
+		
 	}
 
 	private static void Switch_nameId(String nameid) {
@@ -44,7 +67,7 @@ public class iframe_task {
 		
 	}
 
-	private static void Switch_To_WebElement(WebElement ele) {
+	private static void Switch_To_WebElement(WebElement ele) throws InterruptedException {
 		// TODO Auto-generated method stub
 		
 		WebDriver driver = new ChromeDriver();
@@ -54,12 +77,23 @@ public class iframe_task {
        
         
         
-       // WebElement iframe =  driver.findElement(By.id("//iframe[@id=\"mce_0_ifr\"]"));
         driver.switchTo().frame(ele);
         
-        driver.findElement(By.xpath("//div[@class=\"tox-notification tox-notification--in tox-notification--warning\"]")).click();
+        driver.findElement(By.xpath("//button[@class=\"tox-notification__dismiss tox-button tox-button--naked tox-button--icon\"]")).click();
         
         driver.findElement(By.id("tinymce")).clear();
+        Thread.sleep(2000);
+        
+        driver.findElement(By.id("tinymce")).sendKeys("Zakriya");
+        Thread.sleep(2000);
+        
+        
+        
+        
+       // driver.findElement(By.id("tinymce")).sendKeys("ZR Zakir");
+       
+        
+        driver.quit();
         
    
         
