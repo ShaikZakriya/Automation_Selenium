a9960ddbcc711d4eb54e055aa2cd15d493529da9
Mustaq Ali Baig <mustaqalibaig3698@gmail.com>
2025-05-06 17:31:11 +0530

    Mustaq
    Switching to Frame

---
+++ src/main/java/Constructors_practice.java
@@ -0,0 +1,14 @@
+
+public class Constructors_practice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		
+		Constructors_practice obj = new Constructors_practice();
+		
+		
+
+	}
+
+}
