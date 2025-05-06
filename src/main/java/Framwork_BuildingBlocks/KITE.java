5b67f64fdd1da879c5ee3dabc2abaf972781ba37
Mustaq Ali Baig <mustaqalibaig3698@gmail.com>
2025-05-06 18:56:17 +0530

    Mustaq
    Constructer

---
+++ src/main/java/Framwork_BuildingBlocks/KITE.java
@@ -0,0 +1,42 @@
+package Framwork_BuildingBlocks;
+
+public class KITE {
+
+	public static void main(String[] args) {
+		
+		
+	KITE obj = new KITE();
+	obj.apple();
+	obj.Bike();
+	obj.car();
+	
+		
+		
+		
+		
+	}
+
+
+  
+   
+   public void apple()
+   {
+	   System.out.println("apple");
+   }
+   
+   public KITE()
+   {
+	   System.out.println("KITE");
+   }
+   
+   public void car()
+   {
+	   System.out.println("car");
+   }
+   
+   public void Bike()
+   {
+	   System.out.println("Bike");
+   }
+   
+}
---
+++ src/main/java/Framwork_BuildingBlocks/KITE_STATIC.java
@@ -0,0 +1,46 @@
+package Framwork_BuildingBlocks;
+
+public class KITE_STATIC {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		KITE_STATIC.Orange();
+		KITE_STATIC.Mango();
+		KITE_STATIC obj = new KITE_STATIC();
+		KITE_STATIC.Apple();
+	
+
+	}
+	
+
+
+
+public static void Orange() {
+	
+	
+	System.out.println("Orange");
+}
+public static void Mango() {
+	
+	System.out.println("Mango");
+	
+}
+
+public KITE_STATIC (){
+	
+	System.out.println("KITE_STATIC");
+	
+}
+
+
+public static void Apple() {
+	
+	System.out.println("Apple");
+	
+}
+
+
+
+}
+
