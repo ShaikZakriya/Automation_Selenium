7ee49fabd8b0b88862ec88f93f0338e4f41d7f4a
Mustaq Ali Baig <mustaqalibaig3698@gmail.com>
2025-05-13 13:45:40 +0530

    mustaq
    Updated

--- src/main/java/utils/BaseClass.java
+++ src/main/java/utils/BaseClass.java
@@ -5,6 +5,7 @@ import java.io.InputStream;
 import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
