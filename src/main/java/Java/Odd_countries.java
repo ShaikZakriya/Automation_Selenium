5aac596eacadf4ffa0b1e21e96d6aee643244579
Aman <amanayaan10@gmail.com>
2025-05-13 13:41:39 +0530

    aman
    updated

--- src/main/java/Java/Odd_countries.java
+++ src/main/java/Java/Odd_countries.java
@@ -11,40 +11,34 @@ import org.openqa.selenium.support.ui.Select;
 
 public class Odd_countries {
 
-		public static void main(String[] args) throws InterruptedException {
-			
-			WebDriver driver = new ChromeDriver();
-			driver.manage().window().maximize(); 
-			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-			driver.get("https://www.globalsqa.com/demo-site/select-dropdown-menu/");
-			
-			WebElement dropdown = driver.findElement(By.xpath("//select"));
-		    
-		    Select sc = new Select(dropdown);
-		    List<WebElement> options = sc.getOptions();
-		    int length = options size();
-		    
-		    for (int i =0; i<=options.size(); i++) {
-		    	
-		   
-		    	if(!(i%2==0));
-		    	
-		    	{
-		    		String str = Alloptions.get(i).getText();
-		    		
-		    		
-		    	}
-		    	
-		    	
-		    	
-		    	
-		    }
-		    		
-		    
-		    
-		  
-		}
-		    
+    public static void main(String[] args) throws InterruptedException {
 
-	}
+        // Setup Chrome WebDriver
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize(); 
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 
+        // Navigate to the demo site
+        driver.get("https://www.globalsqa.com/demo-site/select-dropdown-menu/");
+
+        // Locate the dropdown element
+        WebElement dropdown = driver.findElement(By.xpath("//select"));
+
+        // Use Select class to handle the dropdown
+        Select select = new Select(dropdown);
+        List<WebElement> options = select.getOptions();
+
+        System.out.println("Countries at odd positions:");
+
+        // Loop through the dropdown options and print only those at odd positions (1st, 3rd, 5th, etc.)
+        for (int i = 0; i < options.size(); i++) {
+            if (i % 2 == 0) {  // Even index = Odd position (1st, 3rd, etc.)
+                String country = options.get(i).getText();
+                System.out.println(country);
+            }
+        }
+
+        // Close the browser
+        driver.quit();
+    }
+}
--- src/main/java/Java/facebookLogin.java
+++ src/main/java/Java/facebookLogin.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 
 
-public class facebook {
+public class facebookLogin {
 
 	public static void main(String[] args) throws InterruptedException {
 	
